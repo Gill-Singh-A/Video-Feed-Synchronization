31209efd8878f035c96af67dab2ad83e488463d0
Gill-Singh-A <amansg22@iitk.ac.in>
2024-01-31 02:10:48 +0530

    Implemented Loading of Time Mappings

--- synchronize.py
+++ synchronize.py
@@ -1,4 +1,57 @@
 #! /usr/bin/env python3
 
+import cv2, pickle
+from pathlib import Path
+from datetime import date
+from optparse import OptionParser
+from colorama import Fore, Back, Style
+from time import strftime, localtime, time
+
+status_color = {
+    '+': Fore.GREEN,
+    '-': Fore.RED,
+    '*': Fore.YELLOW,
+    ':': Fore.CYAN,
+    ' ': Fore.WHITE
+}
+
+def display(status, data, start='', end='\n'):
+    print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
+
+def get_arguments(*args):
+    parser = OptionParser()
+    for arg in args:
+        parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
+    return parser.parse_args()[0]
+
+def makeFolder(folder_name):
+    cwd = Path.cwd()
+    folder = cwd / folder_name
+    folder.mkdir(exist_ok=True)
+
 if __name__ == "__main__":
-    pass
+    arguments = get_arguments(('-f', "--feed", "feed", "Name of Feed Folders with time mappings (seperated by ',')"),
+                              ('-w', "--write", "write", "Folder Name to store the Final Video Files"))
+    if not arguments.feed:
+        display('-', f"Please provide Name of Feed Folders with time mappings")
+        exit(0)
+    else:
+        arguments.feed = arguments.feed.split(',')
+    time_mappings = {}
+    display(':', f"Loading Time Mappings")
+    T1 = time()
+    not_found = []
+    for video_feed_folder in arguments.feed:
+        display('*', f"\t{video_feed_folder}", end='')
+        t1 = time()
+        with open(f"{video_feed_folder}/time_mapping", 'rb') as file:
+            time_mappings[video_feed_folder] = pickle.load(file)
+        t2 = time()
+        if t2 == t1:
+            print(f" => {Back.MAGENTA}{t2-t1} seconds{Back.RESET}")
+    T2 = time()
+    display('+', f"Loaded Time Mappings")
+    display(':', f"\tTime Taken     = {Back.MAGENTA}{T2-T1} seconds{Back.RESET}")
+    display(':', f"\tTimings Loaded = {Back.MAGENTA}{len(arguments.feed)}{Back.RESET}")
+    if T2 != T1:
+        display(':', f"\tRate           = {Back.MAGENTA}{len(arguments.feed)/(T2-T1):.2f} timings/second{Back.RESET}")
